70043df2ac827e9c00cb9c7b4ad0053782f95c97
Steven Lee <steven.lee1393@gmail.com>
2020-07-03 20:51:31 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,7 @@
 
 def key_for_min_value(name_hash)
   low_num = 100000000000
-  low_num_key = ""
+  low_num_key = []
   name_hash.each do |key|
     if name_hash{key}<low_num
       low_num = name_hash{key}
